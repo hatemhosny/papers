da87b721cc8c4bb28e6ce9365c70c8f84a200d0b
Hatem Hosny <hatemhosny@gmail.com>
2017-10-13 18:05:52 +0200

    chore: remove pages and unnecessary modules

--- src/app/app-pages/app-pages-routing.module.ts
+++ src/app/app-pages/app-pages-routing.module.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppPagesComponent } from './app-pages.component';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { SampleComponent } from './sample/sample.component';
 
 const routes: Routes = [{
@@ -14,27 +14,6 @@ const routes: Routes = [{
   }, {
     path: 'sample',
     component: SampleComponent,
-  }, {
-    path: 'ui-features',
-    loadChildren: '../pages/ui-features/ui-features.module#UiFeaturesModule',
-  }, {
-    path: 'components',
-    loadChildren: '../pages/components/components.module#ComponentsModule',
-  }, {
-    path: 'maps',
-    loadChildren: '../pages/maps/maps.module#MapsModule',
-  }, {
-    path: 'charts',
-    loadChildren: '../pages/charts/charts.module#ChartsModule',
-  }, {
-    path: 'editors',
-    loadChildren: '../pages/editors/editors.module#EditorsModule',
-  }, {
-    path: 'forms',
-    loadChildren: '../pages/forms/forms.module#FormsModule',
-  }, {
-    path: 'tables',
-    loadChildren: '../pages/tables/tables.module#TablesModule',
   }, {
     path: '',
     redirectTo: 'dashboard',
--- src/app/app-pages/app-pages.module.ts
+++ src/app/app-pages/app-pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { AppPagesComponent } from './app-pages.component';
-import { DashboardModule } from '../pages/dashboard/dashboard.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { AppPagesRoutingModule } from './app-pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
 import { SampleComponent } from './sample/sample.component';
