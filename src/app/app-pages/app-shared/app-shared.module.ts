d6428342c968c85ed030be6919732ae16f217777
Hatem Hosny <hatemhosny@gmail.com>
2017-10-22 00:24:02 +0200

    feat(cards): add flip card component

--- src/app/app-pages/app-shared/app-shared.module.ts
+++ src/app/app-pages/app-shared/app-shared.module.ts
@@ -1,11 +1,16 @@
+import {ThemeModule} from '../../@theme/theme.module';
 import { NgModule } from '@angular/core';
+import { FlipCardComponent } from './flip-card/flip-card.component';
 
 @NgModule({
   imports: [
-
+    ThemeModule,
   ],
   declarations: [
-
+    FlipCardComponent,
   ],
+  exports: [
+    FlipCardComponent,
+  ]
 })
 export class AppSharedModule { }
