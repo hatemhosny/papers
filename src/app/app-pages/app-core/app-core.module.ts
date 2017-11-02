e1e27436c11003145ba0816b4b7cfb758c9b1689
Hatem Hosny <hatemhosny@gmail.com>
2017-11-02 02:24:54 +0200

    feat(breadcrumbs): config, service and component

--- src/app/app-pages/app-core/app-core.module.ts
+++ src/app/app-pages/app-core/app-core.module.ts
@@ -1,7 +1,6 @@
 import { ErrorHandler, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { ToasterModule } from 'angular2-toaster';
-import { BreadcrumbService } from 'ng2-breadcrumb/ng2-breadcrumb';
 
 import { throwIfAlreadyLoaded } from '../../@core/module-import-guard';
 import { AppCoreComponent } from './app-core.component';
@@ -9,6 +8,7 @@ import { NotificationService } from './notification/notification.service';
 import { NotificationComponent } from './notification/notification.component';
 import { LogService } from './log/log.service';
 import { DataService } from './data/data.service';
+import { BreadcrumbsService } from '../app-shared/breadcrumbs/breadcrumbs.service';
 
 @NgModule({
   imports: [
@@ -26,7 +26,7 @@ import { DataService } from './data/data.service';
     LogService,
     { provide: ErrorHandler, useExisting: LogService },
     DataService,
-    BreadcrumbService,
+    BreadcrumbsService,
   ],
 
 })
--- src/app/app-pages/app-shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/app-pages/app-shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,25 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
-import { BreadcrumbService } from 'ng2-breadcrumb/ng2-breadcrumb';
+import { BreadcrumbsService } from './breadcrumbs.service';
+import { breadcrumbsConfig } from './breadcrumbs.config';
 
 @Component({
   selector: 'pfm-breadcrumbs',
-  template: `<pfm-card><breadcrumb prefix="Home"></breadcrumb></pfm-card>`,
+  templateUrl: 'breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() prefix: string = breadcrumbsConfig.prefix;
 
-  constructor(private breadcrumbService: BreadcrumbService) {
+  public isHidden = false;
+  public _urls: string[];
+  public _routerSubscription: any;
+
+  constructor(
+    private router: Router,
+    private breadcrumbService: BreadcrumbsService,
+  ) { }
+
+  ngOnInit(): void {
+    this._urls = new Array();
+
+    if (this.prefix.length > 0) {
+      this._urls.unshift(this.prefix);
+    }
+
+    this._routerSubscription = this.router.events.subscribe((navigationEnd: NavigationEnd) => {
+
+      if (navigationEnd instanceof NavigationEnd) {
+        this._urls.length = 0; // Fastest way to clear out array
+        this.generateBreadcrumbTrail(navigationEnd.urlAfterRedirects ? navigationEnd.urlAfterRedirects : navigationEnd.url);
+
+        this.hideIfNoBreadcrumbs();
+      }
+    });
+
+    breadcrumbsConfig.names.forEach((item) => {
+      this.breadcrumbService.addFriendlyNameForRoute(item.route, item.name);
+    });
+    breadcrumbsConfig.regexNames.forEach((item) => {
+      this.breadcrumbService.addFriendlyNameForRouteRegex(item.route, item.name);
+    });
+    breadcrumbsConfig.hide.forEach((item) => {
+      this.breadcrumbService.hideRoute(item.route);
+    });
+    breadcrumbsConfig.regexHide.forEach((item) => {
+      this.breadcrumbService.hideRouteRegex(item.route);
+    });
+    breadcrumbsConfig.noBreadcrumbs.forEach((item) => {
+      this.breadcrumbService.removeBreadcrumbsRoute(item.route);
+    });
+    breadcrumbsConfig.regexNoBreadcrumbs.forEach((item) => {
+      this.breadcrumbService.removeBreadcrumbsRouteRegex(item.route);
+    });
+
+    this._urls.length = 0;
+    this.generateBreadcrumbTrail(this.router.url);
+
+    this.hideIfNoBreadcrumbs();
   }
 
-  ngOnInit() {
-    // this.breadcrumbService.addFriendlyNameForRoute('/home', 'Home');
-    // this.breadcrumbService.addFriendlyNameForRoute('/papers', 'All papers');
-    // this.breadcrumbService.addFriendlyNameForRouteRegex('/home/users/[0-9]/info', 'Information');
+  ngOnChanges(changes: any): void {
+    if (!this._urls) {
+      return;
+    }
 
-    this.breadcrumbService.hideRoute('/home');
-    // this.breadcrumbService.hideRouteRegex('^/home/secret/[a-zA-Z]');
+    this._urls.length = 0;
+    this.generateBreadcrumbTrail(this.router.url);
+  }
 
+  generateBreadcrumbTrail(url: string): void {
+    if (!this.breadcrumbService.isRouteHidden(url)) {
+      // Add url to beginning of array (since the url is being recursively broken down from full url to its parent)
+      this._urls.unshift(url);
+    }
+
+    if (url.lastIndexOf('/') > 0) {
+      this.generateBreadcrumbTrail(url.substr(0, url.lastIndexOf('/'))); // Find last '/' and add everything before it as a parent route
+    } else if (this.prefix.length > 0) {
+      this._urls.unshift(this.prefix);
+    }
+  }
+
+  navigateTo(url: string): void {
+    this.router.navigateByUrl(url);
+  }
+
+  friendlyName(url: string): string {
+    return !url ? '' : this.breadcrumbService.getFriendlyNameForRoute(url);
+  }
+
+  ngOnDestroy(): void {
+    this._routerSubscription.unsubscribe();
+  }
+
+  private hideIfNoBreadcrumbs() {
+    if (this.breadcrumbService.breadcrumbsRemoved(this.router.url)) {
+      this.isHidden = true;
+    } else {
+      this.isHidden = false;
+    }
   }
 
 }
